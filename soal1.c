dc0e5fe915abe54c8e591707ec2d4fb426e9b0cd
PascalBSyarif <pascalbsyarif@gmail.com>
2026-05-11 08:29:00 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,72 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 5
+ *   Hari dan Tanggal    : Senin, 11 Mei 2026
+ *   Nama (NIM)          : Pascal Baihaqi Syarif (13224027)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : program yang menerima input adjMatrix NxN dan mengeluarkan output derajat vertexnya, vertex dengan derajat tertinggi, dan vertex terisolasi
+ * 
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    return 0;
+int main(){
+    int N;
+    scanf("%d", &N);
+    int adjMatrix[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            //printf("Input [%d][%d]: ", i, j);
+            scanf("%d", &adjMatrix[i][j]);
+        }   
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%d ", adjMatrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    int isolated[N];
+    int maxVal = 0;
+    int maxI;
+    for (int i = 0; i < N; i++)
+    {
+        int degree = 0;
+        for (int j = 0; j < N; j++)
+        {
+            if (adjMatrix[i][j] == 1)
+            {
+                degree++;
+            }
+        }
+        if (degree > maxVal)
+        {
+            maxI = i;
+            maxVal = degree;
+        }
+        if (degree == 0)
+        {
+            isolated[i] = 1;
+        }
+        printf("DEGREE %d %d\n", i, degree);
+    }
+    
+    printf("MAX VERTEX %d\n", maxI);
+    printf("ISOLATED");
+    for (int i = 0; i < N; i++)
+    {
+        if (isolated[i] == 1)
+        {
+            printf(" %d", i);
+        }
+        
+    }
+    
 }
